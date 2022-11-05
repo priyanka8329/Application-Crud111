fbce0a36e2b3158f57f9f0d29cb3b02c6066950c
Priyanka <priyankashaw39034@gmail.com>
2022-11-05 20:45:51 +0530

    Initial commit

---
+++ server/controller/controller.js
@@ -0,0 +1,18 @@
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create=(req,res)=>{
+
+}
+
+exports.find=(req,res)=>{
+
+}
+
+exports.update=(req,res)=>{
+    
+}
+
+exports.delete=(req,res)=>{
+    
+}
